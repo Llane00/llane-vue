60cb974b07258362b63cc9263bd1cb46f507e2dc
Llane00 <bluetinestyle@126.com>
2022-10-08 15:53:41 +0800

    feat: 添加effect stop方法

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,5 +1,7 @@
 class ReactiveEffect {
   private _fn: any;
+  public deps = [];
+  public active = true;
 
   constructor(fn, public scheduler?) {
     this._fn = fn;
@@ -9,6 +11,19 @@ class ReactiveEffect {
     activeEffect = this;
     return this._fn();
   }
+
+  stop() {
+    if (this.active) {
+      cleanupEffect(this)
+      this.active = false
+    }
+  }
+}
+
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect)
+  })
 }
 
 const targetMap = new Map();
@@ -25,7 +40,10 @@ export function track(target, key) {
     depsMap.set(key, dep)
   }
 
-  dep.add(activeEffect)
+  if (activeEffect) {
+    dep.add(activeEffect)
+    activeEffect.deps.push(dep)
+  }
 }
 
 export function trigger(target, key) {
@@ -43,5 +61,11 @@ let activeEffect;
 export function effect(fn, options: any = {}) {
   const _effect = new ReactiveEffect(fn, options.scheduler);
   _effect.run();
-  return _effect.run.bind(_effect);
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
+  return runner;
+}
+
+export function stop(runner) {
+  runner.effect.stop();
 }
